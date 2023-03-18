d8482ef87b12599e5b7c54b337644b3bed668184
nwokolo emmanuel <99542337+nwokoloemmanuel6@users.noreply.github.com>
2023-03-18 18:19:43 +0100

    Update null.tf

--- kubernetes/nginx-controller/null.tf
+++ kubernetes/nginx-controller/null.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "get_nlb_hostname" {
     provisioner "local-exec" {
-        command = "aws eks --region eu-west--2 update-kubeconfig --name hr-dev-eks-demo && kubectl get svc fullnameOverridevalue-contoller.namevalue -n '${kubernetes_namespace.nginx-controller.metadata[0].name}' -o jsonpath='{status.loadBalancer.ingress[*].hostname}' > ${path.module}/lb_hostname.txt"
+        command = "aws eks --region eu-west--2 update-kubeconfig --name hr-dev-eks-demo && kubectl get svc fullnameOverridevalue-contoller.namevalue -n '${kubernetes_namespace.nginx-namespace.metadata[0].name}' -o jsonpath='{status.loadBalancer.ingress[*].hostname}' > ${path.module}/lb_hostname.txt"
     }
     depends_on = [
       helm_release.ingress_nginx
