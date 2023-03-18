a6f0b6bc7a3abc4984f9869cb2cb25f8a432e2e2
nwokolo emmanuel <99542337+nwokoloemmanuel6@users.noreply.github.com>
2023-03-18 21:24:34 +0100

    Update route.tf

--- kubernetes/cert-manager/route.tf
+++ kubernetes/cert-manager/route.tf
@@ -1,3 +1,19 @@
+resource "time_sleep" "wait_for_kubernetes" {
+
+    depends_on = [
+        data.aws_eks_cluster.hr-dev-eks-demo
+    ]
+
+    create_duration = "20s"
+}
+
+data "local_file" "lb_hostname"{
+    filename = "${path.module}/lb_hostname.txt"
+    depends_on = [
+      time_sleep.wait_for_kubernetes
+    ]
+}
+
 resource "aws_route53_zone" "hosted_zone" {
   name = var.domain_name
   tags = {
@@ -12,13 +28,18 @@ locals {
     named = "grafana.${var.domain_name}"
   }
 }
-resource "aws_route53_record" "A-record" {
+
+resource "aws_route53_record" "C-record" {
   for_each        = local.instances
   allow_overwrite = true
   zone_id         = aws_route53_zone.hosted_zone.zone_id
   name            = each.value
   type            = "CNAME"
-  record          = [data.local_file.lb_hostname.content]
+  records          = [data.local_file.lb_hostname.content]
+
+  depends_on = [
+    data.local_file.lb_hostname
+  ]
 
 }
 
@@ -37,13 +58,16 @@ resource "aws_acm_certificate" "acm_certificate" {
 }
 
 # get details about a route 53 hosted zone
-data "aws_route53_zone" "route53_zone" {
+/* data "aws_route53_zone" "route53_zone" {
   name         = var.domain_name
   private_zone = false
-}
+} */
 
 # create a record set in route 53 for domain validatation
 resource "aws_route53_record" "route53_record" {
+    depends_on = [
+      aws_route53_record.C-record
+    ]
 
   for_each = {
     for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
@@ -58,7 +82,7 @@ resource "aws_route53_record" "route53_record" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.route53_zone.zone_id
+  zone_id         = aws_route53_zone.hosted_zone.zone_id
 }
 
 # validate acm certificates
@@ -66,4 +90,3 @@ resource "aws_acm_certificate_validation" "acm_certificate_validation" {
   certificate_arn         = aws_acm_certificate.acm_certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
 }
-
